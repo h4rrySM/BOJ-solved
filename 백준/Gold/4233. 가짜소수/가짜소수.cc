2c89bc422ce29a716be1e384891782506b59070e
h4rry-github <smharryahn@gmail.com>
2024-07-24 18:04:37 +0900

    [Gold V] Title: 가짜소수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/4233. 가짜소수/가짜소수.cc
+++ 백준/Gold/4233. 가짜소수/가짜소수.cc
@@ -1,26 +1,3 @@
 #include <iostream>
-#define IO ios::sync_with_stdio(0);cin.tie(0);
-#define ll long long
-using namespace std;
-ll power(ll a, ll b, ll mod){
-    return b ? power(a * a % mod, b >> 1, mod) * (b & 1 ? a : 1) % mod : 1;
-}
-bool prime(ll p){
-    for(int i = 2; i * i <= p; i++){
-        if(p % i == 0){
-            return 0;
-        }
-    }
-    return 1;
-}
-int main(){ IO;
-    ll p, a;
-    for(;;){
-        cin >> p >> a;
-        if(p == 0 && a == 0){
-            break;
-        }
-        cout << (prime(p) ? "no" : (power(a, p, p) == a ? "yes" : "no")) << '\n';
-    }
-    return 0;
-}
+#define l long long
+l p,a;l f(l u,l v){return v?f(u*u%p,v>>1)*(v&1?u:1)%p:1;}l g(l i){for(;i*i<=p;){if(0==p%i++)return 0;}return 1;}main(){for(;std::cin>>p>>a&&p*a;std::cout<<(f(a,p)-a|g(2)?"no\n":"yes\n"));}
