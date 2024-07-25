558ad6ea2d8274ad4618ca525d3d63f8302c31f0
h4rry-github <smharryahn@gmail.com>
2024-07-26 02:07:52 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -1,9 +1,2 @@
 #include <iostream>
-#define IO ios::sync_with_stdio(0);cin.tie(0);
-using namespace std;
-int main(){ IO;
-    int n, m, k;
-    cin >> n >> m >> k;
-    cout << m / n * k << '\n';
-    return 0;
-}
+main(){int n,m,k;std::cin>>n>>m>>k;std::cout<<m/n*k;}
