b0abdc28aacb3cb9d3c13894f8a5822584636348
h4rry-github <smharryahn@gmail.com>
2024-07-30 19:11:10 +0900

    [Bronze IV] Title: 체육은 수학과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/32025. 체육은 수학과목 입니다/체육은 수학과목 입니다.cc
+++ 백준/Bronze/32025. 체육은 수학과목 입니다/체육은 수학과목 입니다.cc
@@ -1,7 +1,2 @@
 #include <iostream>
-int main(){
-    int h, w;
-    std::cin >> h >> w;
-    std::cout << 50 * (h < w ? h : w) << '\n';
-    return 0;
-}
+main(){int h,w;std::cin>>h>>w;std::cout<<50*(h<w?h:w);}
