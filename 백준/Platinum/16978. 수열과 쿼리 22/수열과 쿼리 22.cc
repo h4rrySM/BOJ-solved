e79fef1d47201f3b85f753fd0274a61fe2a2881a
h4rry-github <smharryahn@gmail.com>
2024-08-03 15:27:08 +0900

    [Platinum IV] Title: 수열과 쿼리 22, Time: 56 ms, Memory: 6808 KB -BaekjoonHub

--- 백준/Platinum/16978. 수열과 쿼리 22/수열과 쿼리 22.cc
+++ 백준/Platinum/16978. 수열과 쿼리 22/수열과 쿼리 22.cc
@@ -5,73 +5,85 @@
 
 using namespace std;
 using ll = long long;
-using pil = pair<int, long long>;
+using pii = pair<int, int>;
 
-struct Q2{
+struct Quary2{
     int k, left, right, index;
 };
+int N;
 vector<ll> segtree, a;
-ll init_segtree(int i, int start, int end){
-    int mid = (start + end) / 2;
-    return segtree[i] = (start == end ? a[start] : init_segtree(i << 1, start, mid) + init_segtree((i << 1) + 1, mid + 1, end));
+
+void init_segtree(){
+    for(int i = 0; i < N; i++){
+        segtree[i + N] = a[i]; 
+    }
+    for(int i = N - 1; i > 0; i--){
+        segtree[i] = segtree[i << 1] + segtree[i << 1 ^ 1];
+    }
 }
-void update_segtree(int i, int start, int end, int update_index, ll update_value){
-    int mid = (start + end) / 2;
-    if(start <= update_index && update_index <= end){
-        segtree[i] += update_value;
-        if(start != end){
-            update_segtree(i << 1, start, mid, update_index, update_value);
-            update_segtree((i << 1) + 1, mid + 1, end, update_index, update_value);
+void update_segtree(int update_index, int update_value){
+    update_index += N;
+    segtree[update_index] += update_value;
+    for(update_index >>= 1; update_index > 0; update_index >>= 1){
+        segtree[update_index] = segtree[update_index << 1] + segtree[update_index << 1 ^ 1];
+    }
+}
+ll sum_segtree(int left, int right){
+    ll sum = 0;
+    left += N;
+    right += N;
+    for(; left < right; left >>= 1, right >>= 1){
+        if(left & 1){
+            sum += segtree[left++];
+        }
+        if(!(right & 1)){
+            sum += segtree[right--];
         }
     }
-    return;
-}
-ll sum_segtree(int i, int start, int end, int left, int right){
-    int mid = (start + end) / 2;
-    if(end < left || right < start){
-        return 0;
+    if(left == right){
+        sum += segtree[left];
     }
-    return left <= start && end <= right ? segtree[i] : sum_segtree(i << 1, start, mid, left, right) + sum_segtree((i << 1) + 1, mid + 1, end, left, right);
-}
-bool compare(Q2 a, Q2 b){
-    return a.k < b.k;
+    return sum;
 }
 int main(){ IO;
-    int N, M, q, k, s, t, q2_index = 0;
-    ll v;
+    int M, k, i1, i2, q2_index = 0;
     vector<ll> ans;
-    vector<pil> quary1;
-    vector<Q2> quary2;
+    vector<pii> quary1;
+    vector<Quary2> quary2;
     cin >> N;
     a.resize(N);
-    segtree.resize(4 * N);
+    segtree.resize(2 * N);
     for(ll &ai: a){
         cin >> ai;
     }
     cin >> M;
     for(int i = 0; i < M; i++){
-        cin >> q;
-        if(q == 1){
-            cin >> s >> v;
-            quary1.push_back({s - 1, v});
+        cin >> i1;
+        if(i1 == 1){
+            cin >> i1 >> i2;
+            quary1.push_back({i1 - 1, i2});
         }else{
-            cin >> k >> s >> t;
-            quary2.push_back({k, s - 1, t - 1, q2_index++});
+            cin >> k >> i1 >> i2;
+            quary2.push_back({k, i1 - 1, i2 - 1, q2_index++});
         }
     }
     ans.resize(quary2.size());
-    sort(quary2.begin(), quary2.end(), compare);
-    init_segtree(1, 0, N - 1);
+    sort(quary2.begin(), quary2.end(), 
+        [](Quary2 a, Quary2 b) -> bool {
+            return a.k < b.k;
+        }
+    );
+    init_segtree();
     int j = 0;
     for(int i = 0; i < (int)quary1.size(); i++){
         for(; quary2[j].k == i; j++){
-            ans[quary2[j].index] = sum_segtree(1, 0, N - 1, quary2[j].left, quary2[j].right);
+            ans[quary2[j].index] = sum_segtree(quary2[j].left, quary2[j].right);
         }
-        update_segtree(1, 0, N - 1, quary1[i].first, quary1[i].second - a[quary1[i].first]);
+        update_segtree(quary1[i].first, quary1[i].second - a[quary1[i].first]);
         a[quary1[i].first] = quary1[i].second;
     }
     for(int i = j; i < (int)quary2.size(); i++){
-        ans[quary2[i].index] = sum_segtree(1, 0, N - 1, quary2[i].left, quary2[i].right);
+        ans[quary2[i].index] = sum_segtree(quary2[i].left, quary2[i].right);
     }
     for(ll s: ans){
         cout << s << '\n';
