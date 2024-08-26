7abb0b49920fc05f3f0c1e480f51453e3a4735d9
h4rry-github <smharryahn@gmail.com>
2024-08-27 01:53:27 +0900

    [Gold V] Title: 가짜소수, Time: 16 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Gold/4233. 가짜소수/가짜소수.c
@@ -0,0 +1,2 @@
+#define l long long
+l p,a;l f(l u,l v){return v?f(u*u%p,v>>1)*(v&1?u:1)%p:1;}g(i,t){for(;i<=p/i;)t+=!(p%i++);return !t;}main(){for(;scanf("%d%d",&p,&a)*p;printf("%s",f(a,p)-a|g(2,0)?"no\n":"yes\n"));}
