bde7f59175aa5859a4d9c05704eb76f56353e5b6
SM Ahn <smharryahn@gmail.com>
2024-12-24 23:41:17 +0900

    [Gold I] Title: Consecutive Primes, Time: 13460 ms, Memory: 913584 KB, Score: 30 point -BaekjoonHub

--- 백준/Gold/23005. Consecutive Primes/Consecutive Primes.cc
+++ 백준/Gold/23005. Consecutive Primes/Consecutive Primes.cc
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+using ll = unsigned long long;
 using pii = pair<int, int>;
 using vi = vector<int>;
 using vs = vector<string>;
 int main(){ cin.tie(0)->sync_with_stdio(0);
-    ll l = 1000000000;
+    ll l = 1024000000;
     vector<ll> primes;
-    vector<bool> check_prime(l, true);
-    for(ll i = 2; i <= 31622; i++){
+    vector<bool> check_prime(l + 1, true);
+    for(ll i = 2; i <= 32000; i++){
         if(check_prime[i]){
-            for(ll j = i * i; j < l; j += i){
+            for(ll j = i * i; j <= l; j += i){
                 check_prime[j] = false;
             }
         }
@@ -20,14 +20,14 @@ int main(){ cin.tie(0)->sync_with_stdio(0);
             primes.push_back(i);
         }
     }
-    ll z, res = 0;
+    ll z;
     int t;
     cin >> t;
     for(int i = 1; i <= t; i++){
         cin >> z;
-        ll max_product = -1;
-        for(int i = 0; i < primes.size() - 1; i++){
-            ll product = primes[i] * primes[i + 1];
+        ll max_product = 6;
+        for(int j = 0; j < primes.size() - 1; j++){
+            ll product = primes[j] * primes[j + 1];
             if(product <= z){
                 max_product = product;
             }else{
